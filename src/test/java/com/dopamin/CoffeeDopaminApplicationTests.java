2696547b79590aa5857f65cfd7d4a125e0f76ad8
Minjae Kim <minjae.kim@mytrum.com>
2023-01-05 21:35:47 +0900

    Initial commit

---
+++ src/test/java/com/dopamin/CoffeeDopaminApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dopamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeDopaminApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
